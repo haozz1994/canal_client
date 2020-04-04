679c721975f62bc1e71c0c6fb2661410f4f29d18
haozz1994 <haozz0611@163.com>
2020-04-04 23:37:33 +0800

    init canal—_demo

---
+++ src/main/java/com/haozz/canal_client_demo/CanalClientDemoApplication.java
@@ -0,0 +1,13 @@
+package com.haozz.canal_client_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanalClientDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanalClientDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haozz/canal_client_demo/CanalClientDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haozz.canal_client_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanalClientDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
